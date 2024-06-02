78c725d7089ce7431bbe73eb441d7da6eecd6986
Katlego Sibanyoni <sibanyonikatlego09@gmail.com>
2024-06-02 08:42:32 +0000

    Udating Css and Html  Contact Form

--- Learning/html/port.js
+++ Learning/html/port.js
@@ -2,11 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var expandableElement = document.querySelector('.expandable');
     var aboutContentElement = document.getElementById('about-content');
 
-    expandableElement.onclick = function() {
-        if (aboutContentElement.style.display === 'none' || aboutContentElement.style.display === '') {
-            aboutContentElement.style.display = 'block';
-        } else {
-            aboutContentElement.style.display = 'none';
-        }
-    }
+    expandableElement.addEventListener('click', function() {
+        aboutContentElement.classList.toggle('visible');
+    });
 });
